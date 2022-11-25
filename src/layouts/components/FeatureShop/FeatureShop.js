87ecf78e5f79c4990a2e899985a74dd224453bb1
Pham Hai Nam <namphamhai7@gmail.com>
2022-11-25 22:31:38 +0700

    Create Feature Shop in Footer

---
+++ src/layouts/components/FeatureShop/FeatureShop.js
@@ -0,0 +1,12 @@
+import classNames from "classnames/bind";
+import styles from './FeatureShop.module.scss';
+
+import { useState, useEffect } from "react";
+
+const cx = classNames.bind(styles);
+
+function FeatureShop () {
+
+}
+
+export default FeatureShop;
